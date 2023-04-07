78cbd2dbcb91c7e3f934ce0283efb4f3eb414cba
ZILLEALI <zilleali1245@gmail.com>
2023-04-07 23:23:04 +0500

    Create composer-setup.php

---
+++ composer-setup.php
@@ -0,0 +1,22 @@
+<?php
+    $url = 'https://getcomposer.org/installer';
+    $sig = 'A4BAA88C2BDBA2EAE0D31FA2DC0D644448E109AA';
+    $sigfile = 'composer-setup.sig';
+    $composerfile = 'composer-setup.php';
+
+    if (file_exists($sigfile)) {
+        $signature = trim(file_get_contents($sigfile));
+    } else {
+        $signature = '';
+    }
+
+    if (hash('SHA384', file_get_contents($composerfile)) === $sig) {
+        echo 'Installer verified';
+    } else {
+        echo 'Installer corrupt';
+        unlink($composerfile);
+        exit(1);
+    }
+
+    $command = "php $composerfile";
+    exec($command);
